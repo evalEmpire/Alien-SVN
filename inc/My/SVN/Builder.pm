3efb2b8b3728abb7133069799b2a3f3194ac5442
Michael G Schwern <schwern@pobox.com>
2007-09-12 10:08:17 +0000

    r35875@windhund:  schwern | 2007-09-12 03:06:33 -0700  Put in the necessary fobs to get the installation parameters from the  Build.PL to the subversion configure and SVN::Core Makefile.PL.

--- inc/My/SVN/Builder.pm
+++ inc/My/SVN/Builder.pm
@@ -7,6 +7,7 @@ use Cwd;
 sub _run {
     my($prog, @args) = @_;
     
+    print "Running $prog @args\n";
     return system($prog, @args) == 0 ? 1 : 0;
 }
 
@@ -15,8 +16,65 @@ sub _chdir_to_svn {
     chdir 'src/subversion-1.4.5' or die $!;
 }
 
+sub _chdir_to_native {
+    _chdir_to_svn;
+    chdir "subversion/bindings/swig/perl/native" or die $!;
+}
+
 sub _chdir_back {
     chdir $Orig_CWD;
+}
+
+
+my %build_to_makemaker = (
+   # Some names they have in common
+   map {lc($_) => $_} qw(DESTDIR PREFIX INSTALL_BASE UNINST INSTALLDIRS),
+);
+
+my %translate_values = (
+    installdirs     => {
+        core    => "perl",
+        site    => 'site',
+        vendor  => 'vendor'
+    }
+);
+
+sub _makemaker_args {
+    my $self = shift;
+
+    my $props = $self->{properties};
+    my %mm_args;
+
+    for my $key (keys %build_to_makemaker) {
+        next unless defined $props->{$key};
+        my $value = $props->{$key};
+        $value = $translate_values{$key}{$value} if $translate_values{$key};
+
+        $mm_args{$build_to_makemaker{$key}} = $value;
+    }
+    
+    return map { "$_=$mm_args{$_}" } keys %mm_args;
+}
+
+sub _configure_args {
+    my $self = shift;
+
+    my $props = $self->{properties};
+    my $prefix = $props->{install_base} || $props->{prefix};
+    
+    return() unless $prefix;
+    return "--prefix=$prefix";
+}
+
+sub _run_svn_configure {
+    my $self = shift;
+    
+    _chdir_to_svn;
+    
+    _run("./configure", $self->_configure_args)
+        or do { warn "configuring SVN failed";      return 0 };
+    
+    _chdir_back;
 }
 
 sub ACTION_code {
@@ -25,33 +83,55 @@ sub ACTION_code {
     $self->SUPER::ACTION_code;
 
     _chdir_to_svn;
+
+    _run("make swig-pl-lib")
+        or do { warn "building swig-pl-lib failed"; return 0 };
+
+    _chdir_back;
+    _chdir_to_native;
+
+    _run("perl", "Makefile.PL", $self->_makemaker_args)
+        or do { warn "running Makefile.PL failed"; return 0 };
+    _run("make") or do { warn "building SV::Core failed"; return 0 };
     
-    _run("./configure")     or do { warn "configuring SVN failed";      return 0 };
-    _run("make swig-pl")    or dp { warn "building SVN::Core failed";   return 0 };
+    _chdir_back;
+    _chdir_to_svn;
+    
+    _run("make swig-pl")
+        or do { warn "building swig-pl failed"; return 0 };
     
     _chdir_back;
     
     return 1;
 }
 
+
 sub ACTION_test {
     my $self = shift;
     
-    _chdir_to_svn;
+    _chdir_to_native;
     
-    my $test_status = _run("make check-swig-pl");
+    my $test_status = _run("make test");
     
     _chdir_back;
     
     return $test_status;
 }
 
+
 sub ACTION_install {
     my $self = shift;
     
     _chdir_to_svn;
+
+    _run("make install-swig-pl-lib")
+        or do { warn "installing swig-pl-lib failed"; return 0 };
+
+    _chdir_back;
+    _chdir_to_native;
     
-    _run("make install-swig-pl") or do { warn "installing SVN::Core failed";  return 0 };
+    _run("make install")
+        or do { warn "installing SVN::Core failed"; return 0 };
     
     _chdir_back;
     
