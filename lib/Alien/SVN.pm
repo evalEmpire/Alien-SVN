1c2624a70e051073caf10ea25608b818f3eefcfd
Michael G Schwern <schwern@pobox.com>
2007-09-12 10:08:06 +0000

    r35873@windhund:  schwern | 2007-09-12 03:04:32 -0700  A bit of documentation and the necessary licensing info for Subversion.

--- lib/Alien/SVN.pm
+++ lib/Alien/SVN.pm
@@ -6,4 +6,20 @@ use version;  our $VERSION = qw(1.4.5.0);
 
 Alien::SVN - A wrapper for installing the SVN Perl bindings
 
+=head1 DESCRIPTION
+
+Alien::SVN is a wrapper to install the Perl bindings for subversion, also known as L<SVN::Core>.  If your module needs L<SVN::Core> it can depend on L<Alien::SVN> instead and then the CPAN shell can handle automatic installation.
+
+It comes with a copy of Subversion 1.4.5 which it will compile but only installs the Perl bindings.  B<Subversion itself will not be installed>.
+
+=head1 LICENSE
+
+Alien::SVN is copyright 2007 Michael G Schwern <schwern@pobox.com> and is
+licensed under the same terms as Perl itself.  See L<http://www.perl.com/perl/misc/Artistic.html> for licensing.
+
+Subversion and SVN::Core are copyright (c) 2000-2006 CollabNet (http://www.colabnet.net).
+All rights reserved.  See L<http://subversion.tigris.org/license-1.html> for licensing.
+
+=cut
+
 1;
