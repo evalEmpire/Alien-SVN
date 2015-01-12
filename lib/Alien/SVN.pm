eccb272ed0fd02bfec076cb064825cac0e007655
Michael G. Schwern <schwern@pobox.com>
2015-01-12 15:05:41 -0800

    Update URLs and legal information in the docs.

--- lib/Alien/SVN.pm
+++ lib/Alien/SVN.pm
@@ -24,30 +24,29 @@ subversion binaries will not be installed>.
 
 Alien::SVN only wraps SVN::Core.  We don't work on it.
 
-Bug reports, problems and feedback about Alien::SVN and building
-SVN::Core should come to us at E<lt>bug-Alien-SVNE<gt> at
-E<lt>rt.cpan.orgE<gt>.  Or use the web interface at
-L<http://rt.cpan.org>.
+Bug reports, problems and feedback about the Alien::SVN distribution
+and building SVN::Core should come to us at
+L<https://github.com/evalEmpire/Alien-SVN/issues>.
 
-Issues and improvements to SVN::Core should go to the Subversion
-project directly.  L<http://subversion.apache.org/issue-tracker.html>
+Issues and improvements to SVN::Core should go to the Apache Subversion
+project directly.  L<https://subversion.apache.org/reporting-issues.html>
 
 If you're not sure who to report to, you can always report to us and
 we'll point you in the right direction.
 
 Report early, report often.
 
-The repository can be found at F<http://github.com/schwern/alien-svn/tree/master>.
+The repository can be found at F<https://github.com/evalEmpire/Alien-SVN>.
 
 =head1 LICENSE
 
-Alien::SVN is copyright 2007-2009 Michael G Schwern E<lt>schwern@pobox.comE<gt>
+Alien::SVN is copyright 2007-2015 Michael G Schwern E<lt>schwern@pobox.comE<gt>
 It is licensed under the same terms as Perl itself.
 See L<http://www.perl.com/perl/misc/Artistic.html> for licensing.
 
-This product includes software developed by CollabNet (L<http://www.Collab.Net/>).
-Subversion and SVN::Core are copyright (c) 2000-2009 CollabNet L<http://www.colabnet.net>.
-All rights reserved.  See L<http://subversion.tigris.org/license-1.html> for licensing.
+This product includes Apache Subversion licensed under the Apache
+License 2.0 or similar terms.  See F<src/subversion/LICENSE> for full
+licensing information.
 
 =cut
 
